2015e8606af18bbf1ef14b0c72be0910d134e40c
Soumyadeep Rakshit <111779423+soum-rakshit@users.noreply.github.com>
2024-05-19 22:09:53 +0530

    added theme codes

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,14 +1,73 @@
-import express from "express";
-import { getFeedPosts, getUserPosts, likePost} from "../controllers/posts.js"
-import { verifyToken } from "../middleware/auth.js";
+import Post from "../models/post.js";
+import User from "../models/User.js";
 
-const router = express.Router();
+/* Create*/
+export const createPost = async (req, res) => {
+    try {
+        const { userId, description, picturePath } = req.body;
+        const user = await User.findById(userId);
+        const newPost = new Post({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            description,
+            userPicturePath: user.picturePath,
+            picturePath,
+            likes: {},
+            comments: [],
+        });
+        await newPost.save();
+
+        const post = await Post.find();
+
+        res.status(201).json(post);
+    } catch (errr) {
+        res.status(409).json({ message: errr.message });
+    }
+};
 
 /*Read*/
-router.get("/", verifyToken, getFeedPosts);
-router.get("/:userId/posts", verifyToken, getUserPosts);
+export const getFeedPosts = async (req, res) => {
+    try {
+        const post = await Post.find();
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const getUserPosts = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const post = await Post.find({ userId });
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
 
 /*Update*/
-router.patch("/:id/like", verifyToken, likePost);
+export const likePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post = await Post.findById({ id });
+        const isLiked = post.likes.get({ userId });
 
-export default router;
+        if (isLiked) {
+            post.likes.delete(userId);
+        } else {
+            post.likes.delete(userId, true);
+        }
+
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            { likes: post.likes },
+            { new: true }
+        );
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
